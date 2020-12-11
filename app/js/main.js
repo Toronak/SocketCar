867ae6d976a40f7d0783441a62b864a8aef56fb2
Roman Romaschenko <toronakcollect@gmail.com>
2020-12-11 15:59:58 +0200

    in work

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,28 @@ for (let i = 0; i < btns.length; i++) {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
-    document.querySelector(".header__wrapper").style.padding = "20px 30px 20px 30px";
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".header__wrapper").style.margin = "1.04% 20.31% 1.04% 20.31%";
     document.querySelector(".header").style.background = "#ffffff";
   } else {
-    document.querySelector(".header__wrapper").style.padding = "78px 30px 10px 30px";
+    document.querySelector(".header__wrapper").style.margin = "4.06% 20.31% 1.04% 20.31%";
     document.querySelector(".header").style.background = "#E9E9E9";
   }
+}
+
+// Modal window
+let modal = document.getElementById('myModal');
+let btn = document.querySelectorAll("myBtn");
+let span = document.getElementsByClassName("close-icon")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
