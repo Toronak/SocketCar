d40c1feb8f3f08e2d62e58add8de7453d35bb4f8
Roman Romaschenko <toronakcollect@gmail.com>
2020-12-18 11:27:01 +0200

    Update

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,73 @@
 //---Change language---
-
-let btns = document.querySelectorAll(".btn-language");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    let current = document.querySelector(".active");
-    current.className = current.className.replace(" active", "");
-    this.className += " active";
-  });
+let changeLanguage = document.querySelectorAll(".btn-language");
+// for (let i = 0; i < changeLanguage.length; i++) {
+//   changeLanguage[i].addEventListener("click", function() {
+//     let current = document.getElementsByClassName("active");
+//     current.className = current.className.replace(" active","");
+//     this.className =+ " active";
+//   });
+// }
+changeLanguage.onclick = function change(){
+if(changeLanguage.classList("active")){
+  changeLanguage.classList.remove("active");
+} else{
+  changeLanguage.classList.add("active");
 }
-// //---Sticky header---
+}
+//---Sticky header---
 window.onscroll = function() {scrollFunction()};
 
+let header = document.querySelector(".header");
+
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.querySelector(".header__wrapper").style.margin = "1.04% 20.31% 1.04% 20.31%";
-    document.querySelector(".header").style.background = "#ffffff";
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    header.classList.add("header-sticky");
   } else {
-    document.querySelector(".header__wrapper").style.margin = "4.06% 20.31% 1.04% 20.31%";
-    document.querySelector(".header").style.background = "#E9E9E9";
+    header.classList.remove("header-sticky");
   }
 }
 
-// Modal window
-let modal = document.getElementById('myModal');
-let btn = document.querySelectorAll("myBtn");
-let span = document.getElementsByClassName("close-icon")[0];
+// ---Modal window---
+let modal = document.querySelector(".modal");
+let btn = document.querySelector('.btn-order');
+let btn1 = document.querySelector(".redline-top-btn");
+let btn2 = document.querySelector(".redline-bot-btn");
+let bodyLock = document.querySelector("body");
+let closeWindow = document.querySelector(".close-icon");
 
 btn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
+  bodyLock.style.overflowY = "hidden";
+  bodyLock.style.paddingRight = "17px";
 }
-span.onclick = function() {
+btn1.onclick = function() {
+  modal.style.display = "block";
+  bodyLock.style.overflowY = "hidden";
+  bodyLock.style.paddingRight = "17px";
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+  bodyLock.style.overflowY = "hidden";
+  bodyLock.style.paddingRight = "17px";
+}
+closeWindow.onclick = function() {
     modal.style.display = "none";
+    bodyLock.style.overflowY = "auto";
+    bodyLock.style.paddingRight = "0";
 }
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
+        bodyLock.style.overflowY = "auto";
+        bodyLock.style.paddingRight = "0";
     }
+}
+
+// ---Hamburger-menu---
+  document.querySelector(".hamburger-menu").onclick = function(){
+  document.querySelector(".hamburger-row").classList.toggle("hamburger-row-active");
+  document.querySelector(".hamburger__nav-box").classList.toggle("hamburger__nav-box-active");
+  document.querySelector("body").classList.toggle("lock");
+  document.querySelector(".header").classList.toggle("hide");
+  
 }
